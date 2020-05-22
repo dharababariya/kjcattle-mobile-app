12743aeddad7017c1e0b84521560a2d4b4cb58d0
vivek <vivek.koyani@gmail.com>
2020-05-23 00:48:07 +0530

    asyncstorage throw login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,55 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, StatusBar } from 'react-native';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+  Image,
+  Text,
+} from 'react-native';
 import { useTheme } from '@react-navigation/native';
 
-const HomeScreen = ({navigation}) => {
+var { height, width } = Dimensions.get('window');
 
-  const { colors } = useTheme();
+class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataBanner : []
+    }
+  }
 
-  const theme = useTheme();
+  componentDidMount(){
+    const url = 'https://tutofox.com/foodapp/api.json';
+    return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataBanner: responseJson.banner
+      })
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
   
+  render() { 
     return (
       <View style={styles.container}>
-        <StatusBar barStyle= { theme.dark ? "light-content" : "dark-content" }/>
-        <Text style={{color: colors.text}}>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => navigation.navigate("Details")}
-      />
+        <Text>Home Screen</Text>
+        <Text>{JSON.stringify(this.state.dataBanner)}</Text> 
       </View>
     );
-};
-
+  }
+}
+  
 export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
