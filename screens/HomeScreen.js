7e3bc308249e5804febc3da1db178ba24adb2660
vivek <vivek.koyani@gmail.com>
2020-05-24 01:37:10 +0530

    add categories in homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   Image,
   Text,
+  TouchableOpacity,
   ScrollView,
 } from 'react-native';
 import { useTheme } from '@react-navigation/native';
@@ -19,6 +20,8 @@ class HomeScreen extends React.Component {
     super(props);
     this.state = {
       dataBanner: [],
+      dataCategories: [],
+      selectCate: 0,
     };
   }
 
@@ -29,6 +32,7 @@ class HomeScreen extends React.Component {
         this.setState({
           isLoading: false,
           dataBanner: responseJson.banner,
+          dataCategories: responseJson.categories,
         });
       })
       .catch(error => {
@@ -56,14 +60,47 @@ class HomeScreen extends React.Component {
                   <Image
                     style={styles.imagebanner}
                     resizeMode="contain"
-                    source={{uri: itemmap }}
+                    source={{ uri: itemmap }}
                   />
                 );
               })}
             </Swiper>
           </View>
+          <View
+            style={{
+              width: width,
+              borderRadius: 20,
+              paddingVertical: 20,
+              backgroundColor: 'white',
+            }}>
+            <Text style={styles.titleCate}>
+              Categories{this.state.selectCate}
+            </Text>
+            <FlatList
+              horizontal={true}
+              data={this.state.dataCategories}
+              renderItem={({ item }) => this._renderItem(item)}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+          <Text>App Delivery</Text>
+          <Text>{JSON.stringify(this.state.dataCategories)}</Text>
         </View>
       </ScrollView>
+    );
+  }
+  _renderItem(item) {
+    return (
+      <TouchableOpacity
+        style={[styles.divCategories, { backgroundColor: item.color }]}
+        onPress={() => this.setState({ selectCate: item.id })}>
+        <Image
+          style={{ width: 100, height: 80 }}
+          resizeMode="contain"
+          source={{ uri: item.image }}
+        />
+        <Text style={{ fontWeight: 'bold', fontSize: 22 }}>{item.name}</Text>
+      </TouchableOpacity>
     );
   }
 }
@@ -80,5 +117,18 @@ const styles = StyleSheet.create({
     width: width - 40,
     borderRadius: 10,
     marginHorizontal: 20,
+  },
+  divCategories: {
+    backgroundColor: 'red',
+    margin: 5,
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10,
+  },
+  titleCate: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
 });
