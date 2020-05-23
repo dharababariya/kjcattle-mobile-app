78d9816d113a6c8d97d5918301412d671e4a51b6
vivek <vivek.koyani@gmail.com>
2020-05-24 00:46:35 +0530

    add homescreen.js banner and app.js path change

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, AsyncStorage } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import DrawerContent from './DrawerContent';
-import MainTabScreen from './MainTabScreen';
-import RootStackScreen from './RootStackScreen';
+import DrawerContent from './screens/DrawerContent';
+import MainTabScreen from './screens/MainTabScreen';
+import RootStackScreen from '.screens//RootStackScreen';
 import { AuthContext } from './components/context';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-
   // const [isLoading, setIsLoading] = React.useState(true);
   // const [userToken, setUserToken] = React.useState(null);
 
@@ -57,40 +56,56 @@ export default function App() {
     initialLoginState
   );
 
-  const authContext = React.useMemo(() => ({
-    signIn: (userName, password) => {
-      // setUserToken('fgkj');
-      // setIsLoading(false);
-      let userToken;
-      userName = null;
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (userName, password) => {
+        // setUserToken('fgkj');
+        // setIsLoading(false);
+        let userToken;
+        userToken = null;
 
-      if(userName === 'user' && password === 'pass'){
-        userToken = 'dfgdfg';
-      }
-      console.log('usertoken',userToken);
-      dispatch({ type: 'LOGIN', id: userName, token: userToken });
-    },
-    signOut: () => {
-      // setUserToken(null);
-      // setIsLoading(false);
-      dispatch({ type: 'LOGOUT' });
-    },
-    signUp: () => {
-      // setUserToken('fgkj');
-      // setIsLoading(false);
-    },
-  }));
+        if (userName === 'user' && password === 'pass') {
+          try {
+            userToken = 'dfgdfg';
+            await AsyncStorage.setItem('userToken', userToken);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+        console.log('usertoken', userToken);
+        dispatch({ type: 'LOGIN', id: userName, token: userToken });
+      },
+      signOut: async() => {
+        // setUserToken(null);
+        // setIsLoading(false);
+        try {
+            await AsyncStorage.removeItem('userToken');
+          } catch (e) {
+            console.log(e);
+          }
+        dispatch({ type: 'LOGOUT' });
+      },
+      signUp: () => {
+        // setUserToken('fgkj');
+        // setIsLoading(false);
+      },
+    }),
+    []
+  );
 
   useEffect(() => {
-    setTimeout(() => {
+    setTimeout(async () => {
       // setIsLoading(false);
-      dispatch({ type: 'REGISTER', token: 'fgg' });
+      let userToken;
+      userToken = null;
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        console.log(e);
+      }
+      dispatch({ type: 'REGISTER', token: userToken });
     }, 1000);
   }, []);
-
-  
-
-  
 
   if (loginState.isLoading) {
     return (
@@ -114,4 +129,4 @@ export default function App() {
       </NavigationContainer>
     </AuthContext.Provider>
   );
-}
+} 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,49 +7,78 @@ import {
   TextInput,
   Image,
   Text,
+  ScrollView,
 } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import Swiper from 'react-native-swiper';
 
 var { height, width } = Dimensions.get('window');
 
 class HomeScreen extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      dataBanner : []
-    }
+      dataBanner: [],
+    };
   }
 
-  componentDidMount(){
-    const url = 'https://tutofox.com/foodapp/api.json';
-    return fetch(url)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      this.setState({
-        isLoading: false,
-        dataBanner: responseJson.banner
+  componentDidMount() {
+    fetch('https://tutofox.com/foodapp/api.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          dataBanner: responseJson.banner,
+        });
       })
-    }).catch((error) => {
-      console.log(error);
-    })
+      .catch(error => {
+        console.log(error);
+      });
   }
-  
-  render() { 
+
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <Text>{JSON.stringify(this.state.dataBanner)}</Text> 
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={{ width: width, alignItems: 'center' }}>
+            <Image
+              resizeMode="contain"
+              style={{ height: 60, width: width / 2, margin: 10 }}
+              source={{ uri: 'https://tutofox.com/foodapp/foodapp.png' }}
+            />
+            <Swiper
+              style={{ height: width / 2 }}
+              showsButtons={false}
+              autoplay={true}
+              autoplayTimeou={2}>
+              {this.state.dataBanner.map(itemmap => {
+                return (
+                  <Image
+                    style={styles.imagebanner}
+                    resizeMode="contain"
+                    source={{uri: itemmap }}
+                  />
+                );
+              })}
+            </Swiper>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
-  
+
 export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  imagebanner: {
+    height: width / 2,
+    width: width - 40,
+    borderRadius: 10,
+    marginHorizontal: 20,
   },
 });
